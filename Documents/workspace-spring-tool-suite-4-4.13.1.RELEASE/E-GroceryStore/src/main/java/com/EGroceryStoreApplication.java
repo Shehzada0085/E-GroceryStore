065bc35a00a539fd2a495226b6c4b25e03ffb0c0
Shehzada0085 <ayanshaikh7860@gmail.com>
2022-02-08 09:23:32 +0530

    init

---
+++ Documents/workspace-spring-tool-suite-4-4.13.1.RELEASE/E-GroceryStore/src/main/java/com/EGroceryStoreApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGroceryStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGroceryStoreApplication.class, args);
+	}
+
+}
